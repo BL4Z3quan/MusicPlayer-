30a81f720dec574f1756bfb1c39b2dc27707aeb5
unknown <leanhquanngu@gmail.com>
2024-11-24 17:24:36 +0700

    add Ienai feat. asmi

--- Data.js
+++ Data.js
@@ -3,25 +3,31 @@ let allMusic = [
       name: "Trời Giấu Trời Mang Đi",
       artist: "Amee x ViruSs",
       img: "music-1",
-      src: "music-1"
-    },
+      src: "music-1",
+    }
     {
       name: "Giấc Mơ Rất Thơ",
       artist: "Mer",
       img: "music-2",
-      src: "music-2"
+      src: "music-2",
     },
     {
       name: "Tình Thật Tình Mơ",
       artist: "Mer",
       img: "music-3",
-      src: "music-3"
+      src: "music-3",
     },
     {
     name: "Thiên lý ơi",
       artist: "J97",
       img: "music-4",
-      src: "music-4"
+      src: "music-4",
+  }
+  {
+    name :"Ienai feat. asmi",
+    artist:"MIMINARI",
+    img:"music-5",
+    src:"music-5",
   }
   ];
   
